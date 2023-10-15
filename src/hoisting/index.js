8a1c080006a2a8e63ecd64cbcc4b1714c1568bc5
Angie-07 <angiecb1707@gmail.com>
2023-10-15 18:28:41 -0500

    chore: Code and documentation about closures

---
+++ src/hoisting/index.js
@@ -0,0 +1,19 @@
+nameOfDog();
+function nameOfDog(){
+    console.log("el mejor perrito es: " + elmo);
+}
+var elmo; 
+
+//
+
+var a = "Hello global"
+
+function hello() {
+  let b = "Hello function"
+
+  if(true) {
+    let c = "Hello block"
+  }
+}
+
+hello()
